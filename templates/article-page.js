d5e98a100fa4da18bdde4224b1acd5c9cebc36d6
Adam Sobaniec <sobanieca@gmail.com>
2026-01-04 20:24:57 +0000

    chore: giscuss setup

--- templates/article-page.js
+++ templates/article-page.js
@@ -54,15 +54,15 @@ export function articlePage(article, context, navigation = {}) {
 
         const script = document.createElement('script');
         script.src = 'https://giscus.app/client.js';
-        script.setAttribute('data-repo', 'OWNER/REPO');
-        script.setAttribute('data-repo-id', 'YOUR_REPO_ID');
-        script.setAttribute('data-category', 'General');
-        script.setAttribute('data-category-id', 'YOUR_CATEGORY_ID');
+        script.setAttribute('data-repo', 'sobanieca/sobanieca.github.io');
+        script.setAttribute('data-repo-id', 'R_kgDOQmdbKA');
+        script.setAttribute('data-category', 'Giscuss Comments');
+        script.setAttribute('data-category-id', 'DIC_kwDOQmdbKM4C0kgc');
         script.setAttribute('data-mapping', 'pathname');
         script.setAttribute('data-strict', '0');
         script.setAttribute('data-reactions-enabled', '1');
         script.setAttribute('data-emit-metadata', '0');
-        script.setAttribute('data-input-position', 'top');
+        script.setAttribute('data-input-position', 'bottom');
         script.setAttribute('data-theme', giscusTheme);
         script.setAttribute('data-lang', 'en');
         script.setAttribute('data-loading', 'lazy');
